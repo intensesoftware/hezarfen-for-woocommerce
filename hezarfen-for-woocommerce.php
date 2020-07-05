c044804d251873f92fc077fef5d9aa0c5e0a7112
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2020-07-06 00:09:27 +0300

    plugin init was created.

---
+++ hezarfen-for-woocommerce.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Hezarfen For Woocommerce
+Plugin URI: http://intense.com.tr
+Description: Hezarfen, WooCommerce eklentisini Türkiye için daha kullanılabilir kılmayı amaçlar.
+Version: 0.0
+Author: Intense Yazılım Ltd.
+Author URI: http://intense.com.tr
+License: GPL2
+*/
