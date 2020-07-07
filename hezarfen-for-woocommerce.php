193406e62441f10cfa82348352e501e98e2f59cb
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2020-07-07 20:33:48 +0300

    v0.4.2

--- hezarfen-for-woocommerce.php
+++ hezarfen-for-woocommerce.php
@@ -4,7 +4,7 @@
 Plugin Name: Hezarfen For Woocommerce
 Plugin URI: http://intense.com.tr
 Description: Hezarfen, WooCommerce eklentisini Türkiye için daha kullanılabilir kılmayı amaçlar.
-Version: 0.4.1
+Version: 0.4.2
 Author: Intense Yazılım Ltd.
 Author URI: http://intense.com.tr
 License: GPL2
@@ -12,7 +12,7 @@ License: GPL2
 
 defined( 'ABSPATH' ) || exit;
 
-define('WC_HEZARFEN_VERSION', '0.4.1');
+define('WC_HEZARFEN_VERSION', '0.4.2');
 define('WC_HEZARFEN_FILE', __FILE__);
 
 include_once 'includes/Autoload.php';
