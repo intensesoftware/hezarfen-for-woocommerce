fb4aa1baa989a8ca436c1296530a8730faf08cd1
mucahidbal <balmucahid@gmail.com>
2022-01-10 21:48:13 +0300

    feat: added check for city plate number validity.

--- includes/class-mahalle-local.php
+++ includes/class-mahalle-local.php
@@ -32,10 +32,12 @@ class Mahalle_Local {
 	 * @return array
 	 */
 	public static function get_districts( $city_plate_number ) {
-		require WC_HEZARFEN_UYGULAMA_YOLU . 'includes/Data/mahalle/tr-province-districts.php';
+		if ( self::check_city_plate_number( $city_plate_number ) ) {
+			require WC_HEZARFEN_UYGULAMA_YOLU . 'includes/Data/mahalle/tr-province-districts.php';
 
-		if ( isset( $tr_districts[ $city_plate_number ] ) ) {
-			return $tr_districts[ $city_plate_number ];
+			if ( isset( $tr_districts[ $city_plate_number ] ) ) {
+				return $tr_districts[ $city_plate_number ];
+			}
 		}
 
 		return array();
@@ -50,7 +52,7 @@ class Mahalle_Local {
 	 * @return array
 	 */
 	public static function get_neighborhoods( $city_plate_number, $district ) {
-		if ( $city_plate_number ) {
+		if ( self::check_city_plate_number( $city_plate_number ) ) {
 			require WC_HEZARFEN_UYGULAMA_YOLU . "includes/Data/mahalle/tr-neighborhoods/tr-district-nbrhood-$city_plate_number.php";
 
 			if ( isset( $tr_neighborhoods[ $district ] ) ) {
@@ -59,5 +61,20 @@ class Mahalle_Local {
 		}
 
 		return array();
+	}
+
+	/**
+	 * Checks validity of the city plate number.
+	 * 
+	 * @param string $city_plate_number City plate number.
+	 * 
+	 * @return bool
+	 */
+	private static function check_city_plate_number( $city_plate_number ) {
+		return $city_plate_number &&
+		is_string( $city_plate_number ) &&
+		4 === strlen( $city_plate_number ) &&
+		'TR' === substr( $city_plate_number, 0, 2 ) &&
+		is_numeric( substr( $city_plate_number, 2, 2 ) );
 	}
 }
