94032080d93eda9983b1dcb8eb3b224b0dee5b9b
mucahidbal <balmucahid@gmail.com>
2022-02-13 14:04:41 +0300

    refactor: renamed api url constant

--- includes/Autoload.php
+++ includes/Autoload.php
@@ -146,7 +146,7 @@ class Autoload {
 				'wc_hezarfen_ajax_object',
 				array(
 					'ajax_url'        => admin_url( 'admin-ajax.php' ),
-					'api_url'         => WC_HEZARFEN_API_URL,
+					'api_url'         => WC_HEZARFEN_NEIGH_API_URL,
 					'mahalleio_nonce' => wp_create_nonce( 'mahalle-io-get-data' ),
 				)
 			);
