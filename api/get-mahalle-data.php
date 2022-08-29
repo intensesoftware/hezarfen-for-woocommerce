702857745646baf3d9f6af1d1f9ce10ab61e05ef
mucahidbal <balmucahid@gmail.com>
2022-08-29 16:49:22 +0300

    style: fixed some of the PHPCS errors in the get-mahalle-data.php file

--- api/get-mahalle-data.php
+++ api/get-mahalle-data.php
@@ -12,14 +12,14 @@ require dirname( __DIR__ ) . '/includes/class-mahalle-local.php';
  * @param string $requested_data_type Requested data type (district or neighborhood).
  * @param string $city_plate_number City plate number.
  * @param string $district District name for getting neighborhood data.
- * @param bool $return_nbrhood_ids Return neighborhood ids.
+ * @param bool   $return_nbrhood_ids Return neighborhood ids.
  * 
  * @return array|null
  */
 function get_data( $requested_data_type, $city_plate_number, $district = null, $return_nbrhood_ids = true ) {
 	if ( 'district' === $requested_data_type ) {
 		return Mahalle_Local::get_districts( $city_plate_number );
-	} else if ( 'neighborhood' === $requested_data_type && $district ) {
+	} elseif ( 'neighborhood' === $requested_data_type && $district ) {
 		return Mahalle_Local::get_neighborhoods( $city_plate_number, $district, $return_nbrhood_ids );
 	}
 
@@ -27,9 +27,9 @@ function get_data( $requested_data_type, $city_plate_number, $district = null, $
 }
 
 $requested_data_type = isset( $_GET['dataType'] ) ? $_GET['dataType'] : null;
-$city_plate_number = isset( $_GET['cityPlateNumber'] ) ? $_GET['cityPlateNumber'] : null;
-$district = isset( $_GET['district'] ) ? $_GET['district'] : null;
-$return_nbrhood_ids = isset( $_GET['return_nbrhood_ids'] ) ? filter_var( $_GET['return_nbrhood_ids'] , FILTER_VALIDATE_BOOLEAN ) : true;
+$city_plate_number   = isset( $_GET['cityPlateNumber'] ) ? $_GET['cityPlateNumber'] : null;
+$district            = isset( $_GET['district'] ) ? $_GET['district'] : null;
+$return_nbrhood_ids  = isset( $_GET['return_nbrhood_ids'] ) ? filter_var( $_GET['return_nbrhood_ids'], FILTER_VALIDATE_BOOLEAN ) : true;
 
 if ( $requested_data_type && $city_plate_number ) {
 	echo json_encode( get_data( $requested_data_type, $city_plate_number, $district, $return_nbrhood_ids ) );
