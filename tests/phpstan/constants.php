b9552232c852207f3db60a08d07c3d87ae7b3c2f
mucahidbal <balmucahid@gmail.com>
2022-11-20 23:47:46 +0300

    fix: a PHPSTAN error fixed

--- tests/phpstan/constants.php
+++ tests/phpstan/constants.php
@@ -3,3 +3,4 @@
 define('WC_HEZARFEN_UYGULAMA_YOLU', '/some/dirs/wp-content/plugins/hezarfen-for-woocommerce/');
 define('WC_HEZARFEN_NEIGH_API_URL', 'http://example.com/wp-content/plugins/hezarfen-for-woocommerce/api/get-mahalle-data.php');
 define('HEZARFEN_ENCRYPTION_KEY', 'abc');
+define('HEZARFEN_MST_ASSETS_URL', 'http://example.com/wp-content/plugins/hezarfen-for-woocommerce/packages/manual-shipment-tracking/assets/');
