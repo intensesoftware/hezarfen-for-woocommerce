312d81c63cfb3d4f730b626b27e4a2ab222fe1e6
mucahidbal <balmucahid@gmail.com>
2022-11-20 15:44:26 +0300

    chore: constants.php added

---
+++ tests/phpstan/constants.php
@@ -0,0 +1,4 @@
+<?php
+
+define('WC_HEZARFEN_UYGULAMA_YOLU', "/some/dirs/wp-content/plugins/hezarfen-for-woocommerce/");
+define('WC_HEZARFEN_NEIGH_API_URL', "http://example.com/wp-content/plugins/hezarfen-for-woocommerce/api/get-mahalle-data.php");
