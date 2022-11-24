cb8dfc85168e854853e39d98919dde3f2920f776
mucahidbal <balmucahid@gmail.com>
2022-11-24 16:00:39 +0300

    chore: HEZARFEN_MST_PATH added to constants.php

--- tests/phpstan/constants.php
+++ tests/phpstan/constants.php
@@ -4,3 +4,5 @@ define('WC_HEZARFEN_UYGULAMA_YOLU', '/some/dirs/wp-content/plugins/hezarfen-for-
 define('WC_HEZARFEN_NEIGH_API_URL', 'http://example.com/wp-content/plugins/hezarfen-for-woocommerce/api/get-mahalle-data.php');
 define('HEZARFEN_ENCRYPTION_KEY', 'abc');
 define('HEZARFEN_MST_ASSETS_URL', 'http://example.com/wp-content/plugins/hezarfen-for-woocommerce/packages/manual-shipment-tracking/assets/');
+
+define('HEZARFEN_MST_PATH', '/some/dirs/wp-content/plugins/hezarfen-for-woocommerce/packages/manual-shipment-tracking/');
