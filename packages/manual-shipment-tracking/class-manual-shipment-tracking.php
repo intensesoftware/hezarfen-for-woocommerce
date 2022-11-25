0eaf401455e1a4ce91c62ff67f628b6e8f237f54
mucahidbal <balmucahid@gmail.com>
2022-11-25 09:45:21 +0300

    feat: shipment tracking link added to the admin order details page

--- packages/manual-shipment-tracking/class-manual-shipment-tracking.php
+++ packages/manual-shipment-tracking/class-manual-shipment-tracking.php
@@ -137,16 +137,25 @@ class Manual_Shipment_Tracking {
 	 * @return void
 	 */
 	public function order_details( $order ) {
+		$order_id = $order->get_id();
 		?>
 		<br class="clear" />
 		<h4><?php esc_html_e( 'Cargo Informations', 'hezarfen-for-woocommerce' ); ?> <a href="#" class="edit_address"><?php esc_html_e( 'Edit', 'hezarfen-for-woocommerce' ); ?></a></h4>
 		<?php
-		$courier_company = Helper::get_courier_company( $order->get_id() );
-		$tracking_num    = Helper::get_tracking_num( $order->get_id() );
+		$courier_company = Helper::get_courier_company( $order_id );
+		$tracking_num    = Helper::get_tracking_num( $order_id );
+		$tracking_url    = Helper::get_tracking_url( $order_id );
 		?>
 		<div class="address">
 			<p><strong><?php esc_html_e( 'Courier Company', 'hezarfen-for-woocommerce' ); ?>:</strong> <?php echo esc_html( $courier_company ); ?></p>
-			<p><strong><?php esc_html_e( 'Tracking Number', 'hezarfen-for-woocommerce' ); ?>:</strong> <?php echo esc_html( $tracking_num ); ?></p>
+			<p>
+				<strong><?php esc_html_e( 'Tracking Number', 'hezarfen-for-woocommerce' ); ?>:</strong>
+				<?php if ( $tracking_url ) : ?>
+					<a href="<?php echo esc_url( $tracking_url ); ?>" target="_blank"><?php echo esc_html( $tracking_num ); ?></a>
+				<?php else : ?>
+					<?php echo esc_html( $tracking_num ); ?>
+				<?php endif; ?>
+			</p>
 		</div>
 		<div class="edit_address">
 		<?php
