0a5fcb613c3f845550afbd4c6d4284af8d031fbb
mucahidbal <balmucahid@gmail.com>
2022-11-25 15:55:13 +0300

    refactor: My_Account class created

--- packages/manual-shipment-tracking/class-manual-shipment-tracking.php
+++ packages/manual-shipment-tracking/class-manual-shipment-tracking.php
@@ -12,6 +12,7 @@ defined( 'ABSPATH' ) || exit;
 require_once 'class-helper.php';
 require_once 'admin/class-settings.php';
 require_once 'email/class-email.php';
+require_once 'class-my-account.php';
 
 /**
  * Manual Shipment Tracking package main class.
@@ -44,6 +45,7 @@ class Manual_Shipment_Tracking {
 	public function initialize_classes() {
 		new Settings();
 		new Email();
+		new My_Account();
 	}
 
 	/**
@@ -52,8 +54,6 @@ class Manual_Shipment_Tracking {
 	 * @return void
 	 */
 	public function assign_callbacks_to_hooks() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
-
 		add_filter( 'woocommerce_register_shop_order_post_statuses', array( $this, 'register_order_status' ) );
 		add_filter( 'wc_order_statuses', array( $this, 'append_order_status' ) );
 
@@ -61,31 +61,6 @@ class Manual_Shipment_Tracking {
 			add_filter( 'woocommerce_reports_order_statuses', array( $this, 'append_order_status_to_reports' ), 20 );
 			add_action( 'woocommerce_admin_order_data_after_order_details', array( $this, 'admin_order_details' ) );
 			add_action( 'woocommerce_process_shop_order_meta', array( $this, 'order_save' ), PHP_INT_MAX - 1 );
-		}
-
-		if ( 'yes' === get_option( 'hezarfen_mst_show_shipment_tracking_column' ) ) {
-			add_filter( 'woocommerce_account_orders_columns', array( $this, 'add_new_column' ), PHP_INT_MAX - 1 );
-			add_action( 'woocommerce_my_account_my_orders_column_hezarfen-mst-shipment-tracking', array( $this, 'add_tracking_info_to_column' ) );
-		}
-
-		add_action( 'woocommerce_view_order', array( $this, 'customer_order_details_tracking_info' ), 0 );
-	}
-
-	/**
-	 * Enqueues CSS files.
-	 * 
-	 * @return void
-	 */
-	public function enqueue_styles() {
-		global $wp;
-
-		if ( is_account_page() && ! empty( $wp->query_vars['view-order'] ) ) {
-			wp_enqueue_style(
-				'hezarfen_mst_customer_order_details_css',
-				HEZARFEN_MST_ASSETS_URL . 'css/customer-order-details.css',
-				array(),
-				WC_HEZARFEN_VERSION
-			);
 		}
 	}
 
@@ -217,73 +192,6 @@ class Manual_Shipment_Tracking {
 
 			do_action( 'hezarfen_mst_order_shipped', $order );
 		}
-	}
-
-	/**
-	 * Adds tracking information column to My Account > Orders page.
-	 * 
-	 * @param array<string, string> $columns Columns.
-	 * 
-	 * @return array<string, string>
-	 */
-	public function add_new_column( $columns ) {
-		$offset      = 4;
-		$first_part  = array_slice( $columns, 0, $offset );
-		$second_part = array_slice( $columns, $offset );
-
-		return $first_part + array( 'hezarfen-mst-shipment-tracking' => __( 'Tracking Information', 'hezarfen-for-woocommerce' ) ) + $second_part;
-	}
-
-	/**
-	 * Adds tracking information to the "Tracking Information" column in the My Account > Orders page.
-	 * 
-	 * @param \WC_Order $order Order instance.
-	 * 
-	 * @return void
-	 */
-	public function add_tracking_info_to_column( $order ) {
-		$order_id              = $order->get_id();
-		$courier_company_label = Helper::get_courier_company( $order_id, true )['label'];
-		$tracking_num          = Helper::get_tracking_num( $order_id );
-		$tracking_url          = Helper::get_tracking_url( $order_id );
-
-		if ( $courier_company_label ) {
-			printf( '<span style="display: block">%s</span>', esc_html( $courier_company_label ) );
-		}
-
-		if ( $tracking_url ) {
-			printf( '<a href="%s" target="_blank">%s</a>', esc_url( $tracking_url ), esc_html( $tracking_num ) );
-		}
-	}
-
-	/**
-	 * Adds tracking information to the customer order details page.
-	 * 
-	 * @param string|int $order_id Order ID.
-	 * 
-	 * @return void
-	 */
-	public function customer_order_details_tracking_info( $order_id ) {
-		$courier_company_label = Helper::get_courier_company( $order_id, true )['label'];
-		$tracking_num          = Helper::get_tracking_num( $order_id );
-		$tracking_url          = Helper::get_tracking_url( $order_id );
-		?>
-		<div class="hezarfen-mst-tracking-info-wrapper">
-			<h2 class="woocommerce-order-details__title"><?php esc_html_e( 'Tracking Information', 'hezarfen-for-woocommerce' ); ?></h2>		
-			<?php if ( ! empty( $courier_company_label ) || ! empty( $tracking_num ) ) : ?>
-				<h4><?php echo sprintf( '%s: %s', esc_html__( 'Courier Company', 'hezarfen-for-woocommerce' ), esc_html( $courier_company_label ) ); ?></h4>
-				<h4><?php echo sprintf( '%s: %s', esc_html__( 'Tracking Number', 'hezarfen-for-woocommerce' ), esc_html( $tracking_num ) ); ?></h4>
-
-				<?php if ( $tracking_url ) : ?>
-					<h4><a class="hezarfen-mst-tracking-url" href="<?php echo esc_url( $tracking_url ); ?>" target="_blank"><?php esc_html_e( 'Click here to find out where your cargo is.', 'hezarfen-for-woocommerce' ); ?></a></h4>
-					<?php 
-				endif;
-			else : 
-				?>
-				<p><?php esc_html_e( "Your order hasn't been shipped yet.", 'hezarfen-for-woocommerce' ); ?></p>
-			<?php endif; ?>
-		</div>
-		<?php
 	}
 
 	/**
---
+++ packages/manual-shipment-tracking/class-my-account.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Contains the My_Account class.
+ * 
+ * @package Hezarfen\ManualShipmentTracking
+ */
+
+namespace Hezarfen\ManualShipmentTracking;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Adds new features to the My Account pages.
+ */
+class My_Account {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+
+		if ( 'yes' === get_option( 'hezarfen_mst_show_shipment_tracking_column' ) ) {
+			add_filter( 'woocommerce_account_orders_columns', array( $this, 'add_new_column' ), PHP_INT_MAX - 1 );
+			add_action( 'woocommerce_my_account_my_orders_column_hezarfen-mst-shipment-tracking', array( $this, 'add_tracking_info_to_column' ) );
+		}
+
+		add_action( 'woocommerce_view_order', array( $this, 'customer_order_details_tracking_info' ), 0 );
+	}
+
+	/**
+	 * Adds tracking information column to My Account > Orders page.
+	 * 
+	 * @param array<string, string> $columns Columns.
+	 * 
+	 * @return array<string, string>
+	 */
+	public function add_new_column( $columns ) {
+		$offset      = 4;
+		$first_part  = array_slice( $columns, 0, $offset );
+		$second_part = array_slice( $columns, $offset );
+
+		return $first_part + array( 'hezarfen-mst-shipment-tracking' => __( 'Tracking Information', 'hezarfen-for-woocommerce' ) ) + $second_part;
+	}
+
+	/**
+	 * Adds tracking information to the "Tracking Information" column in the My Account > Orders page.
+	 * 
+	 * @param \WC_Order $order Order instance.
+	 * 
+	 * @return void
+	 */
+	public function add_tracking_info_to_column( $order ) {
+		$order_id              = $order->get_id();
+		$courier_company_label = Helper::get_courier_company( $order_id, true )['label'];
+		$tracking_num          = Helper::get_tracking_num( $order_id );
+		$tracking_url          = Helper::get_tracking_url( $order_id );
+
+		if ( $courier_company_label ) {
+			printf( '<span style="display: block">%s</span>', esc_html( $courier_company_label ) );
+		}
+
+		if ( $tracking_url ) {
+			printf( '<a href="%s" target="_blank">%s</a>', esc_url( $tracking_url ), esc_html( $tracking_num ) );
+		}
+	}
+
+	/**
+	 * Adds tracking information to the customer order details page.
+	 * 
+	 * @param string|int $order_id Order ID.
+	 * 
+	 * @return void
+	 */
+	public function customer_order_details_tracking_info( $order_id ) {
+		$courier_company_label = Helper::get_courier_company( $order_id, true )['label'];
+		$tracking_num          = Helper::get_tracking_num( $order_id );
+		$tracking_url          = Helper::get_tracking_url( $order_id );
+		?>
+		<div class="hezarfen-mst-tracking-info-wrapper">
+			<h2 class="woocommerce-order-details__title"><?php esc_html_e( 'Tracking Information', 'hezarfen-for-woocommerce' ); ?></h2>		
+			<?php if ( ! empty( $courier_company_label ) || ! empty( $tracking_num ) ) : ?>
+				<h4><?php echo sprintf( '%s: %s', esc_html__( 'Courier Company', 'hezarfen-for-woocommerce' ), esc_html( $courier_company_label ) ); ?></h4>
+				<h4><?php echo sprintf( '%s: %s', esc_html__( 'Tracking Number', 'hezarfen-for-woocommerce' ), esc_html( $tracking_num ) ); ?></h4>
+
+				<?php if ( $tracking_url ) : ?>
+					<h4><a class="hezarfen-mst-tracking-url" href="<?php echo esc_url( $tracking_url ); ?>" target="_blank"><?php esc_html_e( 'Click here to find out where your cargo is.', 'hezarfen-for-woocommerce' ); ?></a></h4>
+					<?php 
+				endif;
+			else : 
+				?>
+				<p><?php esc_html_e( "Your order hasn't been shipped yet.", 'hezarfen-for-woocommerce' ); ?></p>
+			<?php endif; ?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Enqueues CSS files.
+	 * 
+	 * @return void
+	 */
+	public function enqueue_styles() {
+		global $wp;
+
+		if ( is_account_page() && ! empty( $wp->query_vars['view-order'] ) ) {
+			wp_enqueue_style(
+				'hezarfen_mst_customer_order_details_css',
+				HEZARFEN_MST_ASSETS_URL . 'css/customer-order-details.css',
+				array(),
+				WC_HEZARFEN_VERSION
+			);
+		}
+	}
+}
