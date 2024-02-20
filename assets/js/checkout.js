c3f609544c4963f239fb8159e29f36356e6ff5ea
Mustafa Şükrü Kapusuz <m@mskapusuz.com>
2024-02-20 06:25:08 +0300

    feat: new wp filter hook(hezarfen_checkout_should_notify_neighborhood_changed) added

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,6 +1,10 @@
 var wc_hezarfen_checkout = {
     mbgb_plugin_active: typeof wc_hezarfen_mbgb_backend !== 'undefined',
     should_notify_neighborhood_changed: function (type) {
+        if( wc_hezarfen_ajax_object.should_notify_neighborhood_changed ) {
+            return true;
+        }
+
         return this.mbgb_plugin_active && (wc_hezarfen_mbgb_backend.address_source === type || (wc_hezarfen_mbgb_backend.address_source === 'shipping' && !this.ship_to_different_checked()))
     },
     ship_to_different_checked: function () {
