da0ddceaba3b5621d6ef862cf767fb0c99c979b9
Mustafa Şükrü Kapusuz <m@mskapusuz.com>
2025-09-13 10:04:13 +0300

    privacy

--- hezarfen-for-woocommerce.php
+++ hezarfen-for-woocommerce.php
@@ -34,6 +34,9 @@ define( 'WC_HEZARFEN_NEIGH_API_URL', plugin_dir_url( __FILE__ ) . 'api/get-mahal
 
 add_action( 'plugins_loaded', 'hezarfen_load_plugin_textdomain' );
 
+// Load privacy policy integration
+require_once WC_HEZARFEN_UYGULAMA_YOLU . 'includes/class-privacy-policy.php';
+
 /**
  * Load plugin textdomain
  * 
@@ -61,6 +64,7 @@ function hezarfen_add_settings_link( $links ) {
 
 // Add settings link to plugins page
 add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'hezarfen_add_settings_link' );
+
 
 // Declare our plugin compatible with the Woocommerce HPOS feature.
 add_action(
