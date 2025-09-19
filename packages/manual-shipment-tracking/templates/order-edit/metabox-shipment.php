0050ff16ca3e61fbffd75f263b0f2cede2ed7687
Mustafa Şükrü Kapusuz <m@mskapusuz.com>
2025-09-19 18:14:20 +0300

    fix: order edit screen hepsijet text

--- packages/manual-shipment-tracking/templates/order-edit/metabox-shipment.php
+++ packages/manual-shipment-tracking/templates/order-edit/metabox-shipment.php
@@ -168,7 +168,7 @@ use \Hezarfen\ManualShipmentTracking\Helper;
                             ?>
 
                             <div class="mb-4 flex items-center justify-between">
-                                <span class="text-sm font-medium text-orange-600">intense.com.tr & Hepsijet işbirliği ile Avantajlı Kargo Fiyatları</span>
+                                <span class="text-sm font-medium text-orange-600" style="font-size: 13px !important;">intense.com.tr & Hepsijet işbirliği ile Avantajlı Kargo Fiyatları</span>
                                 <button type="button" id="hepsijet-help-toggle" class="px-3 py-1 bg-blue-100 text-blue-700 text-xs rounded-lg hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2">
                                     <svg class="w-4 h-4 inline-block mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
