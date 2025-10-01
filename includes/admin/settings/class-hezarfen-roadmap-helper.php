7b6f1bff0c39302bd9aeaf4960a775bba7a013fa
Mustafa Şükrü Kapusuz <m@mskapusuz.com>
2025-10-01 13:37:21 +0300

    add KDV

--- includes/admin/settings/class-hezarfen-roadmap-helper.php
+++ includes/admin/settings/class-hezarfen-roadmap-helper.php
@@ -38,7 +38,7 @@ class Hezarfen_Roadmap_Helper {
 			__( 'Banka sanal pos Vakıf Katılım (tek çekim)', 'hezarfen-for-woocommerce' ),
 			__( 'Banka sanal pos Albaraka (tek çekim)', 'hezarfen-for-woocommerce' ),
 			( current_time( 'timestamp' ) < strtotime( '2025-10-20' ) )
-				? __( 'Hepsijet dışında farklı kargolarla da indirimli kargo anlaşması (şu anda Hepsijet&Intense işbirliğiyle 0-4 desi 69,99TL\'ye gönderim yapabiliyorsunuz, alt gönderim limiti olmadan ve kargoyla anlaşma yapmadan. Takip bilgileri ve sipariş durumu da otomatik güncelleniyor. Pro sürüm gerekmiyor. Diğer kargolarla da anlaşma yapılmasını istiyor musunuz?)', 'hezarfen-for-woocommerce' )
+				? __( 'Hepsijet dışında farklı kargolarla da indirimli kargo anlaşması (şu anda Hepsijet&Intense işbirliğiyle 0-4 desi 69,99TL+KDV\'ye gönderim yapabiliyorsunuz, alt gönderim limiti olmadan ve kargoyla anlaşma yapmadan. Takip bilgileri ve sipariş durumu da otomatik güncelleniyor. Pro sürüm gerekmiyor. Diğer kargolarla da anlaşma yapılmasını istiyor musunuz?)', 'hezarfen-for-woocommerce' )
 				: __( 'Hepsijet dışında farklı kargolarla da indirimli kargo anlaşması (şu anda Hepsijet&Intense işbirliğiyle gönderim yapabiliyorsunuz, alt gönderim limiti olmadan ve kargoyla anlaşma yapmadan. Takip bilgileri ve sipariş durumu da otomatik güncelleniyor. Pro sürüm gerekmiyor. Diğer kargolarla da anlaşma yapılmasını istiyor musunuz?)', 'hezarfen-for-woocommerce' ),
 			__( 'Özel sipariş durumları tanımlayabilme', 'hezarfen-for-woocommerce' ),
 			__( 'Kapıda ödemeye ek tutar tanımlayabilme', 'hezarfen-for-woocommerce' ),
