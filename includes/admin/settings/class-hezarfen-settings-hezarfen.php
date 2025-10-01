1aa895ce78e834bd21c8bb642d8561ce3e1ebf5a
Mustafa Şükrü Kapusuz <m@mskapusuz.com>
2025-10-01 13:36:09 +0300

    2.7.4

--- includes/admin/settings/class-hezarfen-settings-hezarfen.php
+++ includes/admin/settings/class-hezarfen-settings-hezarfen.php
@@ -813,7 +813,7 @@ class Hezarfen_Settings_Hezarfen extends WC_Settings_Page {
 					<?php esc_html_e( 'Hezarfen, bugün 2bin+ site tarafından kullanılıyor ve her geçen gün büyüyor. Hezarfen’in gelecek büyük versiyonu olan 3.0 için özellik geliştirme planlarımızı yaparken sizin de geri bildiriminizi almak istedik. Hezarfen’e birden fazla dikeyde geliştirerek, bazı özellikler için farklı eklenti kullanma ihtiyaçlarını ortadan kaldırmayı ve bu sayede WooCommerce altyapılarının stabilitesini arttırmayı amaçlıyoruz.', 'hezarfen-for-woocommerce' ); ?>
 				</p>
 				<p style="font-size: 14px; color: #666; margin-top: 10px;">
-					<?php esc_html_e( 'Hangi özelliklerin geliştirilmesini istersiniz? Her kategoriden en fazla 5 özellik seçebilirsiniz.', 'hezarfen-for-woocommerce' ); ?>
+					<?php esc_html_e( 'Hangi özelliklerin geliştirilmesini istersiniz? Her kategoriden en fazla 5 özellik seçebilirsiniz. Toplanan veriler geliştirme planlamamıza ışık tutacaktır, nihai karar ekibimiz tarafından değerlendirilecektir. Şimdiden teşekkür ederiz!', 'hezarfen-for-woocommerce' ); ?>
 				</p>
 			</div>
 
--- readme.txt
+++ readme.txt
@@ -6,7 +6,7 @@ Requires at least: 5.3
 Tested up to: 6.8
 Requires PHP: 7.0
 License: GPL2
-Stable tag: 2.7.3
+Stable tag: 2.7.4
 
 🚀 2 bin site kullanıyor! Kargo takip, ücretsiz Hepsijet Entegrasyonu (1-4 desi: 69,99TL+KDV), Mesafeli Satış Sözleşmesi, NetGSM sipariş ve kargo SMS
 == Description ==
@@ -258,6 +258,10 @@ HepsiJET, Yurtiçi Kargo, Sürat Kargo, Aras Kargo, PTT Kargo, Trendyol Express,
 15. Hesabım sayfasında siparişe ait oluşmuş sözleşmelerin gösterilmesi
 
 == Changelog ==
+= 2.7.4 - 2025-10-01 =
+* Yol haritası anketi iyileştirildi.
+* Admin ekranında bazı alanlardaki tekrarlayan yazı problemleri giderildi.
+
 = 2.7.3 - 2025-10-01 =
 * Tekrar yol haritası için oylama yapılmasına izin verilmesi problemi giderildi.
 
